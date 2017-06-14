d7d21c81fc19ccc09b5308fb040e2b6a411952ba
steven <stevenle2011@gmail.com>
2017-06-14 11:57:39 +0700

    remove unnecessary items

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,7 @@ var CompressionPlugin = require("compression-webpack-plugin");
 module.exports = {
   entry: {index:'./src/index.js',
          regLog:'./src/regLog.js',
-          myPolls:'./src/myPolls.js',
-          pollChart: './src/pollChart.js'},
+          myPolls:'./src/myPolls.js'},
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'public')
@@ -52,13 +51,13 @@ new webpack.DefinePlugin({
       },
       exclude: [/\.min\.js$/gi] // skip pre-minified libs
     }),
-    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
     new CompressionPlugin({
       asset: "[path].gz[query]",
       algorithm: "gzip",
       test: /\.js$|\.css$|\.html$/,
       threshold: 10240,
-      minRatio: 0
+      minRatio: 0.8
     })
   ]
 };
